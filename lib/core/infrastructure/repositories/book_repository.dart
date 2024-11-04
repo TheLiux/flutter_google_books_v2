dced4e5b12931ea183505f147858dc66f4682bef
Stefano Liuzzo Scorpo <stefano.liuzzo@devmy.it>
2024-11-04 16:09:46 +0100

    feat: implemented book repository

---
+++ lib/core/infrastructure/repositories/book_repository.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_google_books_corsov2/core/domain/entities/books_api_response.dart';
+import 'package:flutter_google_books_corsov2/core/domain/http_client/i_http_client.dart';
+import 'package:flutter_google_books_corsov2/core/domain/repositories/i_book_repository.dart';
+
+class BookRepository implements IBookRepository {
+  final IHttpClient _httpClient;
+
+  BookRepository(this._httpClient);
+
+  @override
+  Future<BooksApiResponse> getBooksByQuery(String query) async {
+    final response = await _httpClient.get(queryParams: {'q': query});
+
+    return response.data;
+  }
+
+  @override
+  Future<BooksApiResponse> getBooksBySubject(String subject) async {
+    final response = await _httpClient.get(queryParams: {'subject': subject});
+
+    return response.data;
+  }
+}
