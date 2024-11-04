c7d400af5e69b77201ac159db3e50e581a823669
Stefano Liuzzo Scorpo <stefano.liuzzo@devmy.it>
2024-11-04 17:08:51 +0100

    feat: implemented logger

--- lib/core/domain/services/book_service/book_service.dart
+++ lib/core/domain/services/book_service/book_service.dart
@@ -1,33 +1,47 @@
 import 'package:flutter_google_books_corsov2/core/domain/entities/book.dart';
+import 'package:flutter_google_books_corsov2/core/domain/logger/logger.dart';
 import 'package:flutter_google_books_corsov2/core/domain/repositories/i_book_repository.dart';
 import 'package:flutter_google_books_corsov2/core/domain/services/book_service/i_book_service.dart';
+import 'package:injectable/injectable.dart';
 
+@Singleton(as: IBookService)
 class BookService implements IBookService {
-  final IBookRepository bookRepository;
+  final IBookRepository _bookRepository;
+  final ILogger _logger;
 
-  BookService({required this.bookRepository});
+  BookService(this._bookRepository, this._logger);
 
   @override
   Future<List<Book>?> getBooksByQuery(String query) async {
     try {
-      final books = await bookRepository.getBooksByQuery(query);
+      final books = await _bookRepository.getBooksByQuery(query);
 
       return books.items;
-    } catch (e) {
-      //TODO handleException
-      return null;
+    } catch (e, s) {
+      _logger.error(
+        'Book Service - getBooksByQuery',
+        e: e,
+        s: s,
+      );
+
+      rethrow;
     }
   }
 
   @override
   Future<List<Book>?> getBooksBySubject(String subject) async {
     try {
-      final books = await bookRepository.getBooksBySubject(subject);
+      final books = await _bookRepository.getBooksBySubject(subject);
 
       return books.items;
-    } catch (e) {
-      //TODO handleException
-      return null;
+    } catch (e, s) {
+      _logger.error(
+        'Book Service - getBooksByQuery',
+        e: e,
+        s: s,
+      );
+
+      rethrow;
     }
   }
 }
--- lib/core/infrastructure/http_client/http_client.dart
+++ lib/core/infrastructure/http_client/http_client.dart
@@ -1,9 +1,15 @@
 import 'package:flutter_google_books_corsov2/core/domain/http_client/i_http_client.dart';
 import 'package:dio/dio.dart' as dio;
 import 'package:flutter_google_books_corsov2/core/domain/http_client/response.dart';
+import 'package:flutter_google_books_corsov2/core/domain/logger/logger.dart';
+import 'package:injectable/injectable.dart';
 
+@Singleton(as: IHttpClient)
 class HttpClient implements IHttpClient {
   static const String _baseUrl = 'https://www.googleapis.com/books/v1/volumes';
+  final ILogger _logger;
+
+  HttpClient(this._logger);
 
   final _httpClient = dio.Dio(
     dio.BaseOptions(
@@ -16,12 +22,17 @@ class HttpClient implements IHttpClient {
     String? path,
     Map<String, dynamic>? queryParams,
   }) async {
-    final response = await _httpClient.get<T>(
-      path ?? '',
-      queryParameters: queryParams,
-    );
+    try {
+      final response = await _httpClient.get<T>(
+        path ?? '',
+        queryParameters: queryParams,
+      );
 
-    return Response.fromDioResponse<T>(response);
+      return Response.fromDioResponse<T>(response);
+    } catch (e, s) {
+      _logger.error('Http Error - Get', e: e, s: s);
+      rethrow;
+    }
   }
 
   @override
@@ -29,11 +40,16 @@ class HttpClient implements IHttpClient {
     String? path,
     Map<String, dynamic>? queryParams,
   }) async {
-    final response = await _httpClient.post<T>(
-      path ?? '',
-      queryParameters: queryParams,
-    );
+    try {
+      final response = await _httpClient.post<T>(
+        path ?? '',
+        queryParameters: queryParams,
+      );
 
-    return Response.fromDioResponse<T>(response);
+      return Response.fromDioResponse<T>(response);
+    } catch (e, s) {
+      _logger.error('Http Error - Post', e: e, s: s);
+      rethrow;
+    }
   }
 }
