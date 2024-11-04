6f8796a79027aaa5caec5baada69dc6a84c3ee4e
Stefano Liuzzo Scorpo <stefano.liuzzo@devmy.it>
2024-11-04 17:10:51 +0100

    feat: implemented get_it and injectable

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_google_books_corsov2/core/application/book_bloc.dart';
+import 'package:flutter_google_books_corsov2/core/domain/services/book_service/i_book_service.dart';
 import 'package:flutter_google_books_corsov2/core/presentation/screens/home_page.dart';
+import 'package:flutter_google_books_corsov2/injectable.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class App extends StatelessWidget {
@@ -9,10 +13,13 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Google Books',
-      theme: theme.copyWith(textTheme: GoogleFonts.spaceGroteskTextTheme()),
-      home: const HomePage(),
+    return BlocProvider(
+      create: (context) => BookBloc(getIt<IBookService>()),
+      child: MaterialApp(
+        title: 'Flutter Google Books',
+        theme: theme.copyWith(textTheme: GoogleFonts.spaceGroteskTextTheme()),
+        home: const HomePage(),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_google_books_corsov2/app.dart';
+import 'package:flutter_google_books_corsov2/injectable.dart';
 import 'package:json_theme/json_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  configureDependencies();
 
   final themeString = await rootBundle.loadString('assets/theme/theme.json');
   final themeJson = jsonDecode(themeString);
