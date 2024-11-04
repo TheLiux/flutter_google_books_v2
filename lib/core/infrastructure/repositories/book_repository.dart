5b0a26618b8c5f2c9e23e03566a082449ea51b9a
Stefano Liuzzo Scorpo <stefano.liuzzo@devmy.it>
2024-11-04 17:09:56 +0100

    fix: implemented fromJson

--- lib/core/infrastructure/repositories/book_repository.dart
+++ lib/core/infrastructure/repositories/book_repository.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_google_books_corsov2/core/domain/entities/books_api_response.dart';
 import 'package:flutter_google_books_corsov2/core/domain/http_client/i_http_client.dart';
 import 'package:flutter_google_books_corsov2/core/domain/repositories/i_book_repository.dart';
+import 'package:injectable/injectable.dart';
 
+@Singleton(as: IBookRepository)
 class BookRepository implements IBookRepository {
   final IHttpClient _httpClient;
 
@@ -11,13 +13,13 @@ class BookRepository implements IBookRepository {
   Future<BooksApiResponse> getBooksByQuery(String query) async {
     final response = await _httpClient.get(queryParams: {'q': query});
 
-    return response.data;
+    return BooksApiResponse.fromJson(response.data);
   }
 
   @override
   Future<BooksApiResponse> getBooksBySubject(String subject) async {
     final response = await _httpClient.get(queryParams: {'subject': subject});
 
-    return response.data;
+    return BooksApiResponse.fromJson(response.data);
   }
 }
